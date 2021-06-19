7ec1e004fcb30c44df06bd67243e8eb2f44f5277
Andy <84683649+Andy-Smith1@users.noreply.github.com>
2021-06-19 18:12:35 +0100

    Working app, 3 grid options

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const div = document.querySelectorAll('div')
 let newdiv;
 const clearBtn = document.getElementById('clear');
 const rainbowBtn = document.getElementById('rainbow');
+const blackBtn = document.getElementById('black');
 const smallGrid = document.querySelector('.grid16');
 const mediumGrid = document.querySelector('.grid32');
 const largeGrid = document.querySelector('.grid64');
+let gridArea;
 
 
 //CHANGE GRID SIZE
-
 function changeGridSize (gridNumber) {
-    let gridArea = gridNumber * gridNumber;
+        gridArea = gridNumber * gridNumber;
     for (i = 0; i <= gridArea; i++) {
         newDiv = document.createElement('div')
         newDiv.classList.add('gridDiv');
@@ -25,10 +26,16 @@ function changeGridSize (gridNumber) {
     }));
 }
 
-let gridSize16 = changeGridSize;
+// SEPERATE FUNCTION FOR BLACK PEN
+function blackPen () {
+    let gridDiv = document.querySelectorAll('.gridDiv');
+    gridDiv.forEach(gridDiv => gridDiv.addEventListener('mouseover', () => {
+        gridDiv.style.backgroundColor = 'black';
+    }));
+}
+
 
 //FUNCTION FOR RAINBOW BUTTON
-
 function rainbowPen () {
     let gridDiv = gridContainer.querySelectorAll('div')
     gridDiv.forEach(gridDiv => gridDiv.addEventListener('mouseover', () => {
@@ -37,7 +44,6 @@ function rainbowPen () {
 }
 
 //RANDOW COLOR GENERATOR FOR RAINBOW EFFECT
-
 function getRandomColor() {
     let red = Math.floor(Math.random() * 256);
 	let green = Math.floor(Math.random() * 256);
@@ -45,30 +51,46 @@ function getRandomColor() {
 	return `rgb(${red}, ${green}, ${blue})`;
 }
 
-//CLEAR BUTTON
-
+//CLEAR BUTTON FUNCTION
 function clearAll () {
     let gridDiv = gridContainer.querySelectorAll('div')
     gridDiv.forEach(gridDiv => gridDiv.style.backgroundColor = 'white');
 }
 
 // CHOOSE GRID SIZE 
+function gridSize16 () {
+    let currentGrid = document.querySelectorAll('.gridDiv')
+    currentGrid.forEach((div) => {
+        return div.remove();
+    })
+    changeGridSize(16);
+};
 
-// function chooseGridSize () {
-//     const buttons = document.querySelectorAll('button');
-//     if (buttons.classList.contains('grid16')) {
-//         changeGridSize(16);
-//     }
+function gridSize32 () {
+    let currentGrid = document.querySelectorAll('.gridDiv')
+    currentGrid.forEach((div) => {
+        return div.remove();
+    })
+    changeGridSize(32);
+};
 
-// }
+function gridSize64 () {
+    let currentGrid = document.querySelectorAll('.gridDiv')
+    currentGrid.forEach((div) => {
+        return div.remove();
+    })
+    changeGridSize(64);
+};
 
-// changeGridSize(64);
+//DEFAULT GRID SIZE
+changeGridSize(32);
 
 //BUTTON EVENT LISTENERS
-
 clearBtn.addEventListener('click', clearAll);
 rainbowBtn.addEventListener('click', rainbowPen);
-// smallGrid.addEventListener('click', (e));
-// mediumGrid.addEventListener('click', (e));
+blackBtn.addEventListener('click', blackPen)
+smallGrid.addEventListener('click', gridSize16);
+mediumGrid.addEventListener('click', gridSize32);
+largeGrid.addEventListener('click', gridSize64);
 
 
